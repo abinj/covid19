ad69a20584e38dc9c473c9f6bfcd443b953d5c69
abinj <abinmj656@gmail.com>
2020-03-20 14:03:57 -0400

    Patients test cases added to db

--- scripts/canada_covid_updates.py
+++ scripts/canada_covid_updates.py
@@ -23,6 +23,16 @@ for row in tbody.select('tr'):
         provinces.append({"province": row.select('td')[0].text, "confirmed": row.select('td')[1].text
                          , "probable": row.select('td')[2].text, "day": now.day, "month": now.month, "year": now.year})
 
+
+total_tests_table = current_situation_div.select('table')[1]
+tests_tbody = total_tests_table.select('tbody')[0]
+for row in tests_tbody.select('tr'):
+    provinces.append({"total_patients_tested": int((row.select('td')[0].text).replace(',', ""))
+                         , "total_positives": int((row.select('td')[1].text).replace(',', ""))
+                         , "total_negative": int((row.select('td')[2].text).replace(',', ""))
+                         , "day": now.day, "month": now.month, "year": now.year})
+
+
 instance = MongoDB.getInstance()
 instance.create_docs(provinces, "current_situation")
 print(">>>>     Current Situation Scraping Done !!!    >>>>>")
